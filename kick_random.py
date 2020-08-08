6c692db8b4bc51fd262dba25a9ec32d3a132ae8b
Fl1yd <67735535+Fl1yd@users.noreply.github.com>
2020-08-08 15:28:28 +0600

    Add files via upload

---
+++ kick_random.py
@@ -0,0 +1,31 @@
+from .. import loader
+from asyncio import sleep
+import random
+
+
+def register(cb):
+	cb(KickRandomMod())
+
+class KickRandomMod(loader.Module):
+	"""Кик рандом"""
+	strings = {'name': 'KickRandom'}
+
+	async def kickrandcmd(self, event):
+		"""Используй .kickrand, чтобы кикнуть случайного пользователя (может кикнуть вас)."""
+		user = random.choice([i for i in await event.client.get_participants(event.to_id)])
+
+		await event.edit('<b>Кому-то сейчас не повезёт...</b>')
+		await sleep(3)
+
+		# Попытка кика...
+		try:
+			await event.client.kick_participant(event.chat_id, user.id)
+			await sleep(0.5)
+		except:
+			await event.edit('<b>У меня нет достаточных прав :с</b>')
+			return
+
+		# Кто кикнут.
+		name = user.first_name
+		name += " "+user.last_name if user.last_name else ''
+		await event.edit(f"<b>Рандом выбрал <a href=\"tg://user?id={name}\">{user.first_name}</a>, и он кикнут!</b>")
