b037d30eb03da49dab3db26b1cf4ca5d55b747f7
Stas Tirtichniy (Стас Тыртычный) <67735535+Fl1yd@users.noreply.github.com>
2020-09-04 13:04:48 +0600

    Add files via upload

--- calendar.py
+++ calendar.py
@@ -16,7 +16,7 @@ class CalendarMod(loader.Module):
         y, m, d = [int(i) for i in str(date.today()).split("-")]
         year = int(args[0]) if args and args[0].isdigit() else y
         month = int(args[1]) if len(args) == 2 and args[1].isdigit() and int(args[1]) in range(1, 13) else m
-        calen=calendar.month(year, month)
-        if year==y and month==m:
-            calen=calen.replace(str(d),f'</code><strong>{d}</strong><code>')
+        calen = calendar.month(year, month)
+        if year == y and month == m:
+            calen = calen.replace(str(d), f'</code><strong>{d}</strong><code>')
         await event.edit(f"<code>\u2060{calen}</code>")
