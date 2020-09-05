cdcc275c23836c24667c182690a9b4bc32c3d3d9
Stas Tirtichniy (Стас Тыртычный) <67735535+Fl1yd@users.noreply.github.com>
2020-09-05 09:48:50 +0600

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,5 +1,6 @@
 import logging
 from .. import loader, utils
+from asyncio import sleep
 from os import remove
 from telethon import functions
 from telethon.tl.functions.channels import LeaveChannelRequest
@@ -37,11 +38,11 @@ class ChatMod(loader.Module):
         else:
             args = utils.get_args(usrid)
             if not args:
-                return await utils.answer(usrid, '<b>Нет аргумента или реплая.</b>')
+                return await utils.answer(usrid, "<b>Нет аргумента или реплая.</b>")
             try:
                 full = await self.client(GetFullUserRequest(args[0]))
             except ValueError:
-                return await utils.answer(usrid, '<b>Не удалось найти этого пользователя.</b>')
+                return await utils.answer(usrid, "<b>Не удалось найти этого пользователя.</b>")
         logger.debug(full)
         message = ("<b>Имя:</b> <code>{}</code>\n".format(self._handle_string(full.user.first_name)))
         message += ("<b>ID:</b> <code>{}</code>".format(utils.escape_html(full.user.id)))
@@ -60,12 +61,12 @@ class ChatMod(loader.Module):
         to_add_users = utils.get_args_raw(event)
         reply = await event.get_reply_message()
         if not to_add_users and not reply:
-            await event.edit('<b>Нету аргументов.</b>')
+            await event.edit("<b>Нету аргументов.</b>")
         elif reply:
             to_add_users = str(reply.from_id)
         if to_add_users:
             if not event.is_group and not event.is_channel:
-                return await event.edit('<b>Это не чат!</b>')
+                return await event.edit("<b>Это не чат!</b>")
             else:
                 if not event.is_channel and event.is_group:
                     # https://tl.telethon.dev/methods/messages/add_chat_user.html
@@ -80,39 +81,39 @@ class ChatMod(loader.Module):
                                                                                      user_id=userID,
                                                                                      fwd_limit=1000000))
                         except ValueError:
-                            await event.reply('<b>Неверный @ или ID.</b>')
+                            await event.reply("<b>Неверный @ или ID.</b>")
                             return
                         except UserIdInvalidError:
-                            await event.reply('<b>Неверный @ или ID.</b>')
+                            await event.reply("<b>Неверный @ или ID.</b>")
                             return
                         except UserPrivacyRestrictedError:
-                            await event.reply('<b>Настойки приватности пользователя не позволяют пригласить его.</b>')
+                            await event.reply("<b>Настойки приватности пользователя не позволяют пригласить его.</b>")
                             return
                         except UserNotMutualContactError:
-                            await event.reply('<b>Настойки приватности пользователя не позволяют пригласить его.</b>')
+                            await event.reply("<b>Настойки приватности пользователя не позволяют пригласить его.</b>")
                             return
                         except ChatAdminRequiredError:
-                            await event.reply('<b>У меня нету прав.</b>')
+                            await event.reply("<b>У меня нету прав.</b>")
                             return
                         except ChatWriteForbiddenError:
-                            await event.reply('<b>У меня нету прав.</b>')
+                            await event.reply("<b>У меня нету прав.</b>")
                             return
                         except ChannelPrivateError:
-                            await event.reply('<b>У меня нету прав.</b>')
+                            await event.reply("<b>У меня нету прав.</b>")
                             return
                         except UserKickedError:
-                            await event.reply('<b>Пользователь кикнут из чата, обратитесь к администраторам.</b>')
+                            await event.reply("<b>Пользователь кикнут из чата, обратитесь к администраторам.</b>")
                             return
                         except BotGroupsBlockedError:
-                            await event.reply('<b>Бот заблокирован в чате, обратитесь к администраторам.</b>')
+                            await event.reply("<b>Бот заблокирован в чате, обратитесь к администраторам.</b>")
                             return
                         except UserBlockedError:
-                            await event.reply('<b>Пользователь заблокирован в чате, обратитесь к администраторам.</b>')
+                            await event.reply("<b>Пользователь заблокирован в чате, обратитесь к администраторам.</b>")
                             return
                         except InputUserDeactivatedError:
-                            await event.reply('<b>Ошибка! Его аккаунт удалён.</b>')
+                            await event.reply("<b>Ошибка! Его аккаунт удалён.</b>")
                             return
-                    await event.edit('<b>Пользователь приглашён успешно!</b>')
+                    await event.edit("<b>Пользователь приглашён успешно!</b>")
                 else:
                     # https://tl.telethon.dev/methods/channels/invite_to_channel.html
                     for user_id in to_add_users.split(" "):
@@ -125,39 +126,39 @@ class ChatMod(loader.Module):
                             await event.client(functions.channels.InviteToChannelRequest(channel=event.chat_id,
                                                                                          users=[userID]))
                         except ValueError:
-                            await event.reply('<b>Неверный @ или ID.</b>')
+                            await event.reply("<b>Неверный @ или ID.</b>")
                             return
                         except UserIdInvalidError:
-                            await event.reply('<b>Неверный @ или ID.</b>')
+                            await event.reply("<b>Неверный @ или ID.</b>")
                             return
                         except UserPrivacyRestrictedError:
-                            await event.reply('<b>Настойки приватности пользователя не позволяют пригласить его.</b>')
+                            await event.reply("<b>Настойки приватности пользователя не позволяют пригласить его.</b>")
                             return
                         except UserNotMutualContactError:
-                            await event.reply('<b>Настойки приватности пользователя не позволяют пригласить его.</b>')
+                            await event.reply("<b>Настойки приватности пользователя не позволяют пригласить его.</b>")
                             return
                         except ChatAdminRequiredError:
-                            await event.reply('<b>У меня нету прав.</b>')
+                            await event.reply("<b>У меня нету прав.</b>")
                             return
                         except ChatWriteForbiddenError:
-                            await event.reply('<b>У меня нету прав.</b>')
+                            await event.reply("<b>У меня нету прав.</b>")
                             return
                         except ChannelPrivateError:
-                            await event.reply('<b>У меня нету прав.</b>')
+                            await event.reply("<b>У меня нету прав.</b>")
                             return
                         except UserKickedError:
-                            await event.reply('<b>Пользователь кикнут из чата, обратитесь к администраторам.</b>')
+                            await event.reply("<b>Пользователь кикнут из чата, обратитесь к администраторам.</b>")
                             return
                         except BotGroupsBlockedError:
-                            await event.reply('<b>Бот заблокирован в чате, обратитесь к администраторам.</b>')
+                            await event.reply("<b>Бот заблокирован в чате, обратитесь к администраторам.</b>")
                             return
                         except UserBlockedError:
-                            await event.reply('<b>Пользователь заблокирован в чате, обратитесь к администраторам.</b>')
+                            await event.reply("<b>Пользователь заблокирован в чате, обратитесь к администраторам.</b>")
                             return
                         except InputUserDeactivatedError:
-                            await event.reply('<b>Ошибка! Его аккаунт удалён.</b>')
+                            await event.reply("<b>Ошибка! Его аккаунт удалён.</b>")
                             return
-                        await event.edit('<b>Пользователь приглашён успешно!</b>')
+                        await event.edit("<b>Пользователь приглашён успешно!</b>")
 
 
     async def kickmecmd(self, leave):
@@ -166,35 +167,35 @@ class ChatMod(loader.Module):
             await leave.edit("<b>До связи.</b>")
             await bot(LeaveChannelRequest(leave.chat_id))
         except:
-            await leave.edit('<b>Это не чат!</b>')
+            await leave.edit("<b>Это не чат!</b>")
             return
 
 
     async def userscmd(self, message):
-        """Команда .users выводит список всех пользователей в чате."""
+        """Команда .users <имя> выводит список всех пользователей в чате."""
         if message.chat:
             try:
-                await message.edit('<b>Считаем...</b>')
+                await message.edit("<b>Считаем...</b>")
                 info = await message.client.get_entity(message.chat_id)
                 title = info.title if info.title else "this chat"
                 users = await message.client.get_participants(message.chat_id)
-                mentions = f'<b>Пользователей в "{title}": {len(users)}</b> \n'
+                mentions = f"<b>Пользователей в {title}: {len(users)}</b> \n"
                 if not utils.get_args_raw(message):
                     users = await bot.get_participants(message.chat_id)
                     for user in users:
                         if not user.deleted:
-                            mentions += f"\n<a href =\"tg://user?id={user.id}\">{user.first_name}</a> <b>|</b> <code>{user.id}</code>"
+                            mentions += f"\n• <a href =\"tg://user?id={user.id}\">{user.first_name}</a> <b>|</b> <code>{user.id}</code>"
                         else:
-                            mentions += f"\nУдалённый аккаунт <code>{user.id}</code>"
+                            mentions += f"\n• Удалённый аккаунт <b>|</b> <code>{user.id}</code>"
                 else:
                     searchq = utils.get_args_raw(message)
-                    users = await message.client.get_participants(message.chat_id, search=f'{searchq}')
+                    users = await message.client.get_participants(message.chat_id, search=f"{searchq}")
                     mentions = f'<b>В чате "{title}" найдено {len(users)} пользователей с именем {searchq}:</b> \n'
                     for user in users:
                         if not user.deleted:
-                            mentions += f"\n<a href =\"tg://user?id={user.id}\">{user.first_name}</a> <b>|</b> <code>{user.id}</code>"
+                            mentions += f"\n• <a href =\"tg://user?id={user.id}\">{user.first_name}</a> <b>|</b> <code>{user.id}</code>"
                         else:
-                            mentions += f"\nУдалённый аккаунт <code>{user.id}</code>"
+                            mentions += f"\n• Удалённый аккаунт <b>|</b> <code>{user.id}</code>"
             except ChatAdminRequiredError as err:
                 info = await message.client.get_entity(message.chat_id)
                 title = info.title if info.title else "this chat"
@@ -202,86 +203,87 @@ class ChatMod(loader.Module):
                 mentions = f'<b>Пользователей в "{title}": {len(users)}</b> \n'
                 mentions += " " + str(err) + "\n"
         else:
-            await message.edit('<b>Это не чат!</b>')
+            await message.edit("<b>Это не чат!</b>")
             return
         try:
             await message.edit(mentions)
         except MessageTooLongError:
-            await message.edit('<b>Черт, слишком большой чат. Загружаю список пользователей в файл...</b>')
+            await message.edit("<b>Черт, слишком большой чат. Загружаю список пользователей в файл...</b>")
             file = open("userslist.md", "w+")
             file.write(mentions)
             file.close()
             await message.client.send_file(message.chat_id,
                                            "userslist.md",
-                                           caption='<b>Список пользователей в "{}":</b>'.format(title),
+                                           caption="Пользователей в {}:".format(title),
                                            reply_to=message.id)
             remove("userslist.md")
 
 
     async def adminscmd(self, message):
-        """Команда .admins показывает список всех админов в чате."""
+        """Команда .admins <имя> показывает список всех админов в чате."""
         if message.chat:
-            await message.edit('<b>Считаем...</b>')
+            await message.edit("<b>Считаем...</b>")
             info = await message.client.get_entity(message.chat_id)
             title = info.title if info.title else "this chat"
             admins = await message.client.get_participants(message.chat_id, filter=ChannelParticipantsAdmins)
             mentions = f'<b>Админов в "{title}": {len(admins)}</b> \n'
             for user in await message.client.get_participants(message.chat_id, filter=ChannelParticipantsAdmins):
                 if not user.deleted:
-                    link = f"<a href=\"tg://user?id={user.id}\">{user.first_name}</a>"
+                    link = f"• <a href=\"tg://user?id={user.id}\">{user.first_name}</a>"
                     userid = f"<code>{user.id}</code>"
                     mentions += f"\n{link} <b>|</b> {userid}"
                 else:
-                    mentions += f"\nУдалённый аккаунт <code>{user.id}</code>"
+                    mentions += f"\n• Удалённый аккаунт <b>|</b> <code>{user.id}</code>"
             try:
                 await message.edit(mentions, parse_mode="html")
             except MessageTooLongError:
-                await message.edit("<b>Черт, слишком много админов здесь. Загружаю список админов в файл...</b>")
+                await message.edit("Черт, слишком много админов здесь. Загружаю список админов в файл...")
                 file = open("adminlist.md", "w+")
                 file.write(mentions)
                 file.close()
                 await message.client.send_file(message.chat_id,
                                                "adminlist.md",
-                                               caption='<b>Список админов в "{}":</b>'.format(title),
+                                               caption="Админов в {}".format(title),
                                                reply_to=message.id)
                 remove("adminlist.md")
         else:
-            await message.edit('<b>Это не чат!</b>')
+            await message.edit("<b>Я слышал, что только чаты могут иметь админов...</b>")
 
 
     async def botscmd(self, message):
-        """Команда .bots показывает список всех ботов в чате."""
+        """Команда .bots <имя> показывает список всех ботов в чате."""
         if message.chat:
-            await message.edit('<b>Считаем...</b>')
+            await message.edit("<b>Считаем...</b>")
             info = await message.client.get_entity(message.chat_id)
             title = info.title if info.title else "this chat"
             bots = await message.client.get_participants(message.to_id, filter=ChannelParticipantsBots)
             mentions = f'<b>Ботов в "{title}": {len(bots)}</b>\n'
             try:
                 if isinstance(message.to_id, PeerChat):
-                    await message.edit('<b>Я слышал, что только чаты могут иметь ботов...</b>')
+                    await message.edit("<b>Я слышал, что только чаты могут иметь ботов...</b>")
                     return
                 else:
                     async for user in message.client.iter_participants(message.chat_id, filter=ChannelParticipantsBots):
                         if not user.deleted:
-                            link = f"<a href=\"tg://user?id={user.id}\">{user.first_name}</a>"
+                            link = f"• <a href=\"tg://user?id={user.id}\">{user.first_name}</a>"
                             userid = f"<code>{user.id}</code>"
                             mentions += f"\n{link} <b>|</b> {userid}"
                         else:
-                            mentions += f"\nУдалённых ботов <code>{user.id}</code>"
+                            mentions += f"\n• Удалённый бот <b>|</b> <code>{user.id}</code>"
             except ChatAdminRequiredError as err:
                 mentions += " " + str(err) + "\n"
             try:
                 await message.edit(mentions, parse_mode="html")
             except MessageTooLongError:
-                await message.edit("<b>Черт, слишком много ботов здесь. Загружаю список ботов в файл...</b>")
+                await message.edit(
+                    "Черт, слишком много ботов здесь. Загружаю список ботов в файл...")
                 file = open("botlist.md", "w+")
                 file.write(mentions)
                 file.close()
                 await message.client.send_file(message.chat_id,
                                                "botlist.md",
-                                               caption='<b>Список ботов в "{}":</b>'.format(title),
+                                               caption="Ботов в {}:".format(title),
                                                reply_to=message.id)
                 remove("botlist.md")
         else:
-            await message.edit('<b>Я слышал, что только чаты могут иметь ботов...</b>')
+            await message.edit("<b>Я слышал, что только чаты могут иметь ботов...</b>")
