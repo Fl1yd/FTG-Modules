b5d9ed745b96dcde4607a1afb78bc38394125ab2
Stas Tirtichniy (Стас Тыртычный) <67735535+Fl1yd@users.noreply.github.com>
2020-09-05 20:03:57 +0600

    Add files via upload

--- kick_random.py
+++ kick_random.py
@@ -12,17 +12,26 @@ class KickRandomMod(loader.Module):
 
 	async def kickrandcmd(self, event):
 		"""Используй .kickrand, чтобы кикнуть случайного пользователя (может кикнуть вас)."""
-		user = random.choice([i for i in await event.client.get_participants(event.to_id)])
-		await event.edit('<b>Кому-то сейчас не повезёт...</b>')
-		await sleep(3)
+		if event.chat:
+			chat = await event.get_chat()
+			admin = chat.admin_rights
+			creator = chat.creator
+			if not admin and not creator:
+				await event.edit('<b>Я здесь не админ.</b>')
+				return
+			user = random.choice([i for i in await event.client.get_participants(event.to_id)])
+			await event.edit('<b>Кому-то сейчас не повезёт...</b>')
+			await sleep(3)
 
-		try:
-			await event.client.kick_participant(event.chat_id, user.id)
-			await sleep(0.5)
-		except:
-			await event.edit('<b>У меня нет достаточных прав :с</b>')
-			return
+			try:
+				await event.client.kick_participant(event.chat_id, user.id)
+				await sleep(0.5)
+			except:
+				await event.edit('<b>У меня нет достаточных прав :с</b>')
+				return
 
-		name = str(user.first_name)
-		name += " "+user.last_name if user.last_name else ''
-		await event.edit(f"<b>Рандом выбрал <a href=\"tg://user?id={user.id}\">{user.first_name}</a>, и он кикнут!</b>")
+			name = str(user.first_name)
+			name += " "+user.last_name if user.last_name else ''
+			await event.edit(f"<b>Рандом выбрал <a href=\"tg://user?id={user.id}\">{user.first_name}</a>, и он кикнут!</b>")
+		else:
+			await event.edit('<b>Это не чат!</b>')
