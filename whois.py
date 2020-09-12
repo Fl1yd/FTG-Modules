0406c34c48cd84981c694daf9072990987771c9d
Stas Tirtichniy (Стас Тыртычный) <fliyd206@gmail.com>
2020-09-12 09:16:25 +0600

    Update whois.py

--- whois.py
+++ whois.py
@@ -33,17 +33,12 @@ class WhoIsMod(loader.Module):
             message_id_to_reply = None
 
         try:
-            await whos.client.send_file(whos.chat_id, photo,
-                                        caption=caption,
-                                        link_preview=False,
-                                        force_document=False,
-                                        reply_to=message_id_to_reply,
-                                        parse_mode="html")
-
+            await whos.client.send_file(whos.chat_id, photo, caption=caption,
+                                        link_preview=False, force_document=False,
+                                        reply_to=message_id_to_reply, parse_mode="html")
             if not photo.startswith("http"):
                 os.remove(photo)
             await whos.delete()
-
         except TypeError:
             await whos.edit(caption, parse_mode="html")
 
@@ -52,45 +47,36 @@ async def get_user(event):
     """Получение информации о пользователе с реплая или аргумента."""
     if event.reply_to_msg_id and not utils.get_args_raw(event):
         previous_message = await event.get_reply_message()
-        replied_user = await event.client(
-            GetFullUserRequest(previous_message.from_id))
+        replied_user = await event.client(GetFullUserRequest(previous_message.from_id))
     else:
         user = utils.get_args_raw(event)
-
         if user.isnumeric():
             user = int(user)
-
         if not user:
             self_user = await event.client.get_me()
             user = self_user.id
-
         if event.entities is not None:
             probable_user_mention_entity = event.entities[0]
-
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                 user_id = probable_user_mention_entity.user_id
                 replied_user = await event.client(GetFullUserRequest(user_id))
                 return replied_user
         try:
             user_object = await event.client.get_entity(user)
-            replied_user = await event.client(
-                GetFullUserRequest(user_object.id))
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
         except:
             self_user = await event.client.get_me()
             user = self_user.id
             user_object = await event.client.get_entity(user)
-            replied_user = await event.client(
-                GetFullUserRequest(user_object.id))
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
             return replied_user
     return replied_user
 
 
 async def fetch_info(replied_user, event):
     """Подробная информация о пользователе."""
-    replied_user_profile_photos = await event.client(
-        GetUserPhotosRequest(user_id=replied_user.user.id,
-                             offset=42, max_id=0, limit=80))
+    replied_user_profile_photos = await event.client(GetUserPhotosRequest(user_id=replied_user.user.id,
+                                                                          offset=42, max_id=0, limit=80))
     replied_user_profile_photos_count = "Пользователю нужна помощь с загрузкой аватарки."
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
@@ -117,15 +103,10 @@ async def fetch_info(replied_user, event):
         verified = "Нет"
     else:
         verified = "Да"
-    photo = await event.client.download_profile_photo(user_id,
-                                                      str(user_id) + ".jpg",
-                                                      download_big=True)
-    first_name = first_name.replace(
-        "\u2060", "") if first_name else ("Пользователь не указал имя.")
-    last_name = last_name.replace(
-        "\u2060", "") if last_name else ("Пользователь не указал фамилии.")
-    username = "@{}".format(username) if username else (
-        "У пользователя нету юзернейма.")
+    photo = await event.client.download_profile_photo(user_id, str(user_id) + ".jpg", download_big=True)
+    first_name = first_name.replace("\u2060", "") if first_name else ("Пользователь не указал имя.")
+    last_name = last_name.replace("\u2060", "") if last_name else ("Пользователь не указал фамилии.")
+    username = "@{}".format(username) if username else ("У пользователя нету юзернейма.")
     user_bio = "У пользователя нету информации о себе." if not user_bio else user_bio
 
     caption = "<b>ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ:</b>\n\n"
