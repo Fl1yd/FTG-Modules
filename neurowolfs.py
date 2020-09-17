7d7ebf22f2b0fc10dbc2faea561f25a1f88a3f69
Stas Tirtichniy (Стас Тыртычный) <67735535+Fl1yd@users.noreply.github.com>
2020-09-17 20:26:47 +0600

    Upload file

---
+++ neurowolfs.py
@@ -0,0 +1,26 @@
+from .. import loader
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+
+def register(cb):
+    cb(VsratoMod())
+
+class VsratoMod(loader.Module):
+    """Нейроволки"""
+    strings = {'name': 'Нейроволк Акела'}
+
+    async def vsratocmd(self, event):
+        """"Используй .vsrato"""
+        chat = "@neuroakelabot"
+        await event.edit("<b>Минуточку...</b>")
+        async with event.client.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(events.NewMessage(incoming=True, from_users=979556006))
+                await event.client.send_message(chat, 'Дай мне мем, Акела')
+                response = await response
+            except YouBlockedUserError:
+                await event.edit('<b>Разблокируй @neuroakelabot</b>')
+                return
+            await event.client.send_file(event.to_id, response.media)
+        await event.delete()
