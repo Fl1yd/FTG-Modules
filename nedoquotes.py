8d021744897baf31492db228bb5c48cb9caf6f15
Stas Tirtichniy (Стас Тыртычный) <fliyd206@gmail.com>
2020-09-23 13:07:40 +0600

    Create nedoquotes.py

---
+++ nedoquotes.py
@@ -0,0 +1,42 @@
+from .. import loader, utils
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+
+def register(cb):
+    cb(NedoQuotesMod())
+    
+class NedoQuotesMod(loader.Module):
+    """Генератор всратых цитат by @ShittyQuoteBot"""
+    strings = {'name': 'NedoQuotes'}
+
+    async def nqcmd(self, message):
+        """Используй: .nq <текст или реплай>."""
+        chat = "@ShittyQuoteBot"
+        text = utils.get_args_raw(message)
+        reply = await message.get_reply_message()
+        await message.edit("<b>Минуточку...</b>")
+        async with message.client.conversation(chat) as conv:
+            if text:
+                try:
+                    response = conv.wait_event(events.NewMessage(incoming=True, from_users=1389323591))
+                    await message.client.send_message(chat, text)
+                    response = await response
+                except YouBlockedUserError:
+                    await message.edit("<b>Разблокируй @ShittyQuoteBot</b>")
+                    return
+            if reply:
+                try:
+                    user = await utils.get_user(reply)
+                    response = conv.wait_event(events.NewMessage(incoming=True, from_users=1389323591))
+                    await message.client.send_message(chat, f"{reply.raw_text} (с) {user.first_name}")
+                    response = await response
+                except YouBlockedUserError:
+                    await message.edit("<b>Разблокируй @ShittyQuoteBot</b>")
+                    return
+        if response.text:
+            await message.client.send_message(message.to_id, f"<b> {response.text}</b>")
+            await message.delete()
+        if response.media:
+            await message.client.send_file(message.to_id, response.media)
+            await message.delete()
