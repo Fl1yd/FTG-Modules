86a8c4c905f86792d41d597e7520a71e920d0e16
Stas Tirtichniy (Стас Тыртычный) <fliyd206@gmail.com>
2020-10-08 10:56:17 +0600

    Update gsbl.py

--- gsbl.py
+++ gsbl.py
@@ -1,4 +1,5 @@
 import io
+import os
 from .. import loader 
 from PIL import Image
 from gsbl.stick_bug import StickBug
@@ -13,19 +14,21 @@ class GSBLMod(loader.Module):
     
     async def gsblcmd(self, event):
         """Используй .gsbl <реплай на видео>."""
-        reply = await event.get_reply_message() 
-        if not reply:
-            await event.edit("Нет реплая на видео.")
-            return 
-        await event.edit("Минуточку...")
-        im = io.BytesIO()
-        await event.edit("Скачиваю...")
-        await event.client.download_file(reply, im)
-        await event.edit("Обрабатываю...") 
-        im = Image.open(im)
-        sb = StickBug(im)
-        video = sb.video
-        sb.save_video("get_stick_bugged_lol.mp4")
-        await event.edit("Отправляю...")
-        await event.client.send_file(event.to_id, open("get_stick_bugged_lol.mp4", "rb"), reply_to=reply)
-        await event.delete()
+        try:
+            reply = await event.get_reply_message()
+            if not reply:
+                return await event.edit("Нет реплая на видео.")
+            await event.edit("Минуточку...")
+            im = io.BytesIO()
+            await event.edit("Скачиваю...")
+            await event.client.download_file(reply, im)
+            await event.edit("Обрабатываю...")
+            im = Image.open(im)
+            sb = StickBug(im)
+            sb.save_video("get_stick_bugged_lol.mp4")
+            await event.edit("Отправляю...")
+            await event.client.send_file(event.to_id, open("get_stick_bugged_lol.mp4", "rb"), reply_to=reply)
+            os.remove("get_stick_bugged_lol.mp4")
+            await event.delete()
+        except:
+            return await event.edit("Это не картинка/стикер.")
