2ae710c8f92e279d08695beb3f1aaa416c18dde6
Stas Tirtichniy (Стас Тыртычный) <fliyd206@gmail.com>
2020-10-22 08:27:38 +0600

    Update onava.py

--- onava.py
+++ onava.py
@@ -13,39 +13,30 @@ class OnAvaMod(loader.Module):
     async def onavacmd(self, message):
         """Установить на аву гифку/видео/стикер.\nИспользование: .onava <реплай>."""
         try:
-            await message.edit("Скачиваем...")
             reply = await message.get_reply_message()
             if reply:
-                await message.edit("Конвертируем...")
-                if reply.media.document.attributes:
-                    if reply.video:
-                        await message.client.download_media(reply.media, "ava.mp4")
-                        os.system("ffmpeg -i ava.mp4 -c copy -an gifavaa.mp4 -y")
-                        os.system("ffmpeg -i gifavaa.mp4 -vf scale=360:360 gifava.mp4 -y")
-                    else:
-                        await message.client.download_media(reply.media, "tgs.tgs")
-                        os.system("lottie_convert.py tgs.tgs tgs.gif; mv tgs.gif gifava.mp4")
+                await message.edit("Скачиваем...")
+                if reply.video:
+                    await message.client.download_media(reply.media, "ava.mp4")
+                    await message.edit("Конвертируем...")
+                    os.system("ffmpeg -i ava.mp4 -c copy -an gifavaa.mp4 -y")
+                    os.system("ffmpeg -i gifavaa.mp4 -vf scale=360:360 gifava.mp4 -y")
+                else:
+                    await message.client.download_media(reply.media, "tgs.tgs")
+                    await message.edit("Конвертируем...")
+                    os.system("lottie_convert.py tgs.tgs tgs.gif; mv tgs.gif gifava.mp4")
             else:
                 return await message.edit("Нет реплая на гиф/анимированный стикер/видеосообщение.")
             await message.edit("Устанавливаем аву...")
             await message.client(functions.photos.UploadProfilePhotoRequest(video=await message.client.upload_file("gifava.mp4"), video_start_ts=0.0))
             await message.edit("Ава установлена.")
-            try:
-                os.remove("ava.mp4")
-                os.remove("gifava.mp4")
-                os.remove("gifavaa.mp4")
-            except FileNotFoundError:
-                pass
-        except Exception as e:
-            await message.client.send_message(message.to_id, str(e))
+            os.system("rm -rf ava.mp4 gifava.mp4 gifavaa.mp4 tgs*")
+        except:
             await message.edit("Блин, какой я дурак, я не отличаю гифку/анимированный стикер/видео от любого другого файла.\n\n"
                                "<b>ЭТОТ ФАЙЛ НЕ ПОДДЕРЖИВАЕТСЯ!!!</b>(либо просто какая-то тех.ошибка c: )")
             try:
-                os.remove("ava.mp4")
-                os.remove("gifava.mp4")
-                os.remove("gifavaa.mp4")
-                os.remove("tgs.*")
-            except FileNotFoundError:
+                os.system("rm -rf ava.mp4 gifava.mp4 gifavaa.mp4 tgs*")
+            except:
                 pass
             return
 
@@ -63,7 +54,7 @@ class OnAvaMod(loader.Module):
                 await message.client.send_file(message.to_id, "tgs.gif")
                 await message.delete()
                 try:
-                    os.remove("tgs.*")
+                    os.remove("tgs*")
                 except FileNotFoundError:
                     pass
             else:
@@ -72,7 +63,7 @@ class OnAvaMod(loader.Module):
             await message.edit("Блин, какой я дурак, я не отличаю гифку/анимированный стикер/видео от любого другого файла.\n\n"
                                "<b>ЭТОТ ФАЙЛ НЕ ПОДДЕРЖИВАЕТСЯ!!!</b>(либо просто какая-то тех.ошибка c: )")
             try:
-                os.remove("tgs.*")
+                os.remove("tgs*")
             except FileNotFoundError:
                 pass
             return
