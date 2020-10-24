acdebc78298f0c7bc18fa7c2dc62c83f3d0a7400
Stas Tirtichniy (Стас Тыртычный) <67735535+Fl1yd@users.noreply.github.com>
2020-10-24 08:40:53 +0600

    Upload file

---
+++ weather.py
@@ -0,0 +1,32 @@
+import requests
+from .. import loader, utils
+
+
+def register(cb):
+    cb(WeatherMod())
+    
+class WeatherMod(loader.Module):
+    strings = {'name': 'Weather'}
+    
+    async def pwcmd(self, message):
+        """"Кидает погоду картинкой.\nИспользование: .pw <город>; ничего."""
+        args = utils.get_args_raw(message)
+        city = args.replace(' ', '+')
+        if not args:
+            return await message.edit("Ты не указал город.")
+        await message.edit("Узнаем погоду...")
+        if args:
+            city = f"https://wttr.in/{city}.png"
+        await message.client.send_file(message.to_id, city)
+        await message.delete()
+
+
+    async def awcmd(self, message):
+        """Кидает погоду ascii-артом.\nИспользование: .aw <город>; ничего."""
+        city = utils.get_args_raw(message)
+        if not city:
+            return await message.edit("Ты не указал город.")
+        await message.edit("Узнаем погоду...")
+        if city:
+            r = requests.get(f"https://wttr.in/{city}?0?q?T")
+        await message.edit(f"<code>Город: {r.text}</code>")
