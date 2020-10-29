eb8eb23808bfb3b518b41edd8e507da10869e91f
Stas Tirtichniy (Стас Тыртычный) <67735535+Fl1yd@users.noreply.github.com>
2020-10-30 01:30:59 +0600

    Add files via upload

--- weather.py
+++ weather.py
@@ -10,7 +10,7 @@ class WeatherMod(loader.Module):
     strings = {'name': 'Weather'}
     
     async def pwcmd(self, message):
-        """"Кидает погоду картинкой.\nИспользование: .pw <город>."""
+        """"Кидает погоду картинкой.\nИспользование: .pw <город>; ничего."""
         args = utils.get_args_raw(message)
         city = args.replace(' ', '+')
         city = f"https://wttr.in/{city if city != None else ''}.png"
@@ -19,7 +19,7 @@ class WeatherMod(loader.Module):
 
 
     async def awcmd(self, message):
-        """Кидает погоду ascii-артом.\nИспользование: .aw <город>."""
+        """Кидает погоду ascii-артом.\nИспользование: .aw <город>; ничего."""
         city = utils.get_args_raw(message)
         await message.edit("Узнаем погоду...")
         r = requests.get(f"https://wttr.in/{city if city != None else ''}?0?q?T&lang=ru")
