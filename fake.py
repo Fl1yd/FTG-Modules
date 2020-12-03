d33c3e158d0d5ac2159e28a91ae9755a4eacc259
Stas Tirtichniy (Стас Тыртычный) <fliyd206@gmail.com>
2020-12-03 09:15:06 +0600

    fix

--- fake.py
+++ fake.py
@@ -1,41 +1,39 @@
-from random import choice, randint
 from .. import loader, utils
+from random import choice, randint
 from asyncio import sleep
 
 
 def register(cb):
-    cb(FakeMod()) 
+    cb(FakeActionsMod()) 
     
-class FakeMod(loader.Module):
+class FakeActionsMod(loader.Module):
     """Показывает фейковые действия."""
     strings = {'name': 'Fake Actions'} 
     
     async def fakecmd(self, event):
         """Использование: .fake <действие>.\nСписок действий: typing, contact, game, location, record-audio, record-round, record-video, voice, round, video, photo, document.\nОтмена: .fake cancel"""
-        options = ['typing', 'contact', 'game', 'location', 'record-audio', 'record-round',
+        actions = ['typing', 'contact', 'game', 'location', 'record-audio', 'record-round',
                    'record-video', 'voice', 'round', 'video', 'photo', 'document', 'cancel']
-        input_str = utils.get_args_raw(event)
-        args = input_str.split()
+        args = utils.get_args_raw(event).split()
         if len(args) == 0:
-            scam_action = choice(options)
-            scam_time = randint(30, 60)
+            fake_action = choice(actions)
+            fake_time = randint(30, 60)
         elif len(args) == 1:
             try:
-                scam_action = str(args[0]).lower()
-                scam_time = randint(30, 60)
+                fake_action = str(args[0]).lower()
+                fake_time = randint(30, 60)
             except ValueError:
-                scam_action = choice(options)
-                scam_time = int(args[0])
+                fake_action = choice(actions)
+                fake_time = int(args[0])
         elif len(args) == 2:
-            scam_action = str(args[0]).lower()
-            scam_time = int(args[1])
+            fake_action = str(args[0]).lower()
+            fake_time = int(args[1])
         else:
-            await event.edit('<b>Неправильный ввод.</b>')
+            await event.edit('Неправильный ввод.')
             return
         try:
-            if (scam_time > 0):
-                await event.delete()
-                async with event.client.action(event.chat_id, scam_action):
-                    await sleep(scam_time)
+            await event.delete()
+            async with event.client.args(event.chat_id, fake_action):
+                await sleep(fake_time)
         except BaseException:
             return
